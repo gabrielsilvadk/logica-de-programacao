ec730815d21c6644343e5fb1a69cb78dcb737849
gabrielsilvadk <gabriel.silvadk@gmail.com>
2021-08-16 20:32:25 -0300

    desafio array

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace desafio
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for(int i=1; i<=200; i++){
+                if(i%5==0){
+                    Console.WriteLine("Foo");
+                }else if(i%7==0){
+                    Console.WriteLine("Bar");
+                }else if(i%5==0 && i%7==0){
+                    Console.WriteLine("FooBar");
+                }else{
+                    Console.WriteLine(i);
+
+                }
+            }
+        }
+    }
+}
