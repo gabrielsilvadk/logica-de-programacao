076148a1c509b06bd6b7d771a36b0491b7259c38
gabrielsilvadk <gabriel.silvadk@gmail.com>
2021-08-16 22:20:16 -0300

    desafio array

---
+++ arraydesafio/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+public class Program
+{
+    public static void Main()
+    {
+        ImprimeItensCozinha();
+    }
+
+    public static void ImprimeItem(string item)
+    {
+        Console.WriteLine($"{item}");
+    }
+
+    public static void ImprimeItensCozinha()
+    {
+        string[] itens = {"Fogão", "Pia", "Geladeira"};
+        
+       
+        for(int i=0; i<3; i++){
+            Console.WriteLine(itens[i]);
+        }
+    }
+}
